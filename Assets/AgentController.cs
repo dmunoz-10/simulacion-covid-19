dd2ddd8e61355bff71c1d0a389dd5e498a1aae99
Daniel Muñoz <djmv225@gmail.com>
2020-05-29 14:22:21 -0500

    First commit

---
+++ Assets/AgentController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class AgentData{
+    public string state;
+    public int r0 = 3;
+    public int age = 20;
+    public bool isDead = false;
+    public float time = 30f;
+}
+
+public class AgentController : MonoBehaviour{
+    // Start is called before the first frame update
+    public AgentData data;
+    NavMeshAgent myNavMeshAgent;
+    Animator m_Animator;
+    private Image circle;
+    int value = 0;
+    
+    void Start(){
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        m_Animator = GetComponent<Animator>();
+        value = Random.Range(0, SimulationController.points.Length);
+        foreach (Transform child in this.gameObject.transform){
+            if (child.name == "Area"){
+                circle = child.GetChild(0).GetComponent<Image>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (SimulationController.start){
+            if (data.state == "Sick"){
+                data.time -= Time.deltaTime;
+                if (data.time <= 0.0f){
+                    data.state = "Recovered";
+                    circle.color = GetColorRGBA("#2B7800");
+                    SimulationController.recovered += 1;
+                    SimulationController.sick -= 1;
+                }
+            }
+            myNavMeshAgent.SetDestination(SimulationController.points[value].transform.position);
+            float distanceToTarget = Vector3.Distance(transform.position, SimulationController.points[value].transform.position);
+            if (distanceToTarget <= 0.6f){
+                value = Random.Range(0, SimulationController.points.Length);
+            }
+            if (SimulationController.time >= SimulationController.totalTime){
+                myNavMeshAgent.speed = 0.0f;
+                m_Animator.SetBool("Finished", true);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "Agent"){
+            var agent = other.GetComponent<AgentController>().data;
+            var state = agent.state;
+            if ((state == "Sick") && (data.state == "Healthy") && (agent.r0 > 0)){
+                data.state = "Sick";
+                circle.color = GetColorRGBA("#730005");
+                agent.r0 -= 1;
+                SimulationController.sick += 1;
+                SimulationController.healthy -= 1;
+                probabilityDeath();
+            }
+        }
+    }
+
+    Color GetColorRGBA(string hex_color){
+        Color new_color;
+        ColorUtility.TryParseHtmlString(hex_color, out new_color);
+        return new_color;
+    }
+
+    void probabilityDeath(){
+        float percentage = 0;
+        for (int i = 0; i <= 6; i++){
+            if (data.age >= SimulationController.items[i]["min"] && data.age <= SimulationController.items[i]["max"]){
+                percentage = SimulationController.items[i]["percentage"];                
+            }
+            float r = Random.Range(0.0f, 100.0f);
+            if (r <= percentage){
+                data.isDead = true;
+                data.state = "Death"; 
+                m_Animator.SetBool("IsDead", true);
+                myNavMeshAgent.speed = 0.0f;
+                SimulationController.sick -= 1;
+                SimulationController.dead += 1;
+            }
+        }
+    }
+}
---
+++ Assets/SimulationController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using SimpleJSON;
+
+public class SimulationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int healthy, recovered, sick, dead;
+    public static JSONNode items;
+    public static float totalTime = 0f, time = 0f;
+    public static bool start = false;
+    public GameObject[] agents;
+    public Text labelHealthy, labelRecovered, labelSick, labelDead, LabelTime;
+    public InputField agentsField, speedField, timeField;
+    public FreeFlyCamera camera;
+    public static GameObject[] points;
+
+    void Awake(){
+        points = GameObject.FindGameObjectsWithTag("Point");
+        ReadJson();
+    }
+
+    public void StartSimulation(){
+        healthy = int.Parse(agentsField.text);
+        recovered = 0;
+        sick = 1;
+        dead = 0;
+        CreateAgents(healthy, "Healthy");
+        start = true;
+        camera.enabled = true;
+        totalTime = float.Parse(timeField.text) * 60.0f;
+    }
+
+    void CreateAgents(int cant, string state){
+        for (int i = 0; i < cant; i++){
+            GameObject prefab = agents[Random.Range(0, agents.Length)];
+            int value = Random.Range(0, SimulationController.points.Length);
+            GameObject newAgent = Instantiate(prefab, points[value].transform.position, Quaternion.identity);
+            var data = newAgent.GetComponent<AgentController>().data;
+            data.state = state;
+            data.age = Random.Range(0, 100);
+            data.r0 = Random.Range(2,4);
+            newAgent.GetComponent<NavMeshAgent>().speed = float.Parse(speedField.text);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (start && time <= totalTime){
+            Timer();
+        }
+        labelHealthy.text = healthy.ToString();
+        labelRecovered.text = recovered.ToString();
+        labelSick.text = sick.ToString();
+        labelDead.text = dead.ToString();
+    }
+
+    void Timer(){
+        time += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time - (minutes * 60));
+        string niceTime = string.Format("{0:0}:{01:00}", minutes, seconds);
+        LabelTime.text = niceTime;
+    }  
+
+    void ReadJson(){
+        string data = Resources.Load<TextAsset>("mortality").text;
+        items = JSONNode.Parse(data)["items"];
+    }  
+}
